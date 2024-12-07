cb3806ce2438afe0d826dbf0b5eec7d4b54db381
Quynh Anh <Quanha11326@outlook.com.vn>
2024-12-07 23:22:08 +0700

    BTVN tuần 14

---
+++ BTVN_w14.cpp
@@ -0,0 +1,8 @@
+/*
+Cài đặt cây biểu thức cho biểu thức trên lớp ngày 3/12/2024, và in ra biểu thức theo các cách biểu diễn Trung tố, hậu tố, và tiền tố.
+Định nghĩa và khai báo các hàm sau:
+Cài đặt thuộc tính cho 1 node trên cây 
+Cài đặt hàm khởi tạo cây (không được bỏ qua hàm kiểm tra rỗng)
+Cài đặt hàm nhập phần tử (bổ sung) vào cây 
+In các phần tử trên cây theo 3 giải thuật duyệt a*5 - (b*c^6)/d + (h-f)*e^(1/2)
+*/
